25ec1d58154bf4527671410635976b1a8ffdb4b7
Thanh Doan <tcdoan@gmail.com>
2010-10-26 08:54:45 -0500

    code for example 6.10

--- multivariate/johnson/Ex6_9.m
+++ multivariate/johnson/Ex6_9.m
@@ -1,4 +1,4 @@
-% Excercise 6.9
+% Example 6.9
 load Ex6_9.dat;
 X = Ex6_9;
 g = 3; p = 2;
@@ -28,11 +28,16 @@ T = B + W;
 lambda = det(W)/det(T);
 
 % Using table 6.3, p.303, to compute the exact test statistics
-% Can use Barlette correction if n is large!
+
+% Can also use Barlette correction if n is large!
+% barlet_stat = -(n-1-(p+g)/2) * log (det(W)/det(T));
+% crit_value  = chi2inv(0.99,p*(g-1));
+
 test_stat = ((sum(nl_arr) - g - 1)/(g-1))*((1-sqrt(lambda))/sqrt(lambda))
-crit_value = finv(0.99, 2*p, 2*(n-p-2))
-if test_stat <= crit_value  
+crit_value = finv(0.99, 2*(g-1), 2*(n-g-1))
+if test_stat <= crit_value 
     sprintf('No treatment effects. test_stat = %.2f <= %.2f', test_stat, crit_value)
 else   
     sprintf('Treatment difference exist! test_stat = %.2f > %.2f', test_stat, crit_value)    
 end 
+
