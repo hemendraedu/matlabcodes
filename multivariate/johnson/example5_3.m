a16b654e5a13572ed2e7e040c00a57ccef550a63
Thanh Doan <tcdoan@gmail.com>
2010-11-20 12:38:47 -0600

    update ex 5.3 to compute the direction and lengths of ellipse axes

--- multivariate/johnson/example5_3.m
+++ multivariate/johnson/example5_3.m
@@ -1,33 +1,38 @@
 % Example 5.3 - Constructing a confidence ellipse for mean vector mu
-clear all;
-load example_5_3.dat;
-
-% transform 1st and 2nd columns of example_5_3 matrix to near normality 
-X1 = example_5_3(:,1).^(1/4);
-X2 = example_5_3(:,2).^(1/4);
-
-% put X1, X2 into matrix X
-X = [X1, X2];
-
-% n is number of observations,  p is number of features/variables
+clear;
+mydata = load('example5_3.dat');
+X = mydata .^(1/4);
 [n,p] = size(X);
 
-% compute sample mean vector X_bar and sample covariance matrix S
-X_bar = mean(X)';
+X_mean = mean(X)';
 S = cov(X);
 S_inv = inv(S);
 
-% V store eigen-vectors , diagonal elements of D are eigen values
+% V store eigen-vectors , D are eigen values
 [V D] = eig(S);
+display(V); display(D);
+
+% The direction and lengths of axes of ellipsoid
+%   n * (x.mean - mu)'*inv(S)*(x.mean - mu) 
+%       <= c^2 = [p(n-1)/(n-p)]*F(.95, p, n-p)     
+% are:
+ellipse_center = X_mean;
+display(ellipse_center);
+for i = 1:size(V,2)
+    lambda = D(i,i);
+    half_len = sqrt(lambda) * sqrt( (p*(n-1)/(n*(n-p))) * finv(.95,p,n-p) );
+    fprintf('Axis %d, half-length = %10.4f, direction:  \n', i, half_len);
+    disp(V(:,i));    
+end
 
 % compute 95% confidence region
 critvalue = (p*(n-1)/(n-p)) * finv(.95,p,n-p);
 
 % Calculate T_sq to see if [.562, .589 ]' is in the confidence region?
 mu = [.562, .589 ]';
-T_sq = n*(X_bar-mu)'*S_inv*(X_bar-mu);
+T_sq = n*(X_mean-mu)'*S_inv*(X_mean-mu);
 
-mu
+disp(mu);
 if T_sq <= critvalue  
     sprintf('IN confidence region. T square = %.2f <= %.2f', T_sq, critvalue)
 else   
---
+++ multivariate/johnson/example7_10.asv
@@ -0,0 +1,7 @@
+% Example 7.10, Thanh Doan
+data = load('T7-3.DAT');
+n = size(data,1);
+y2 = [301.8, 396.1, 328.2, 307.4, 362.4, 369.5, 229.1]';
+X = [data, y2];
+
+save example7_10.data X -ascii -tabs;
